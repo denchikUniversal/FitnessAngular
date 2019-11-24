512e7518f5fc1b522a035f7f4605f9a4f6b94bc9
denchikUniversal <tkachenkodenys94@gmail.com>
2019-11-24 20:40:43 +0200

    remove all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products.component';
-import { ProductComponent } from './product/product.component';
-import { HomeComponent } from './home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
-    ProductComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
